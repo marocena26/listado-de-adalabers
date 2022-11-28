ed347d346a2eab8e6a6a481931cbfc6d66c47b32
marocena26 <mariaelena.arocena@gmail.com>
2022-11-28 09:51:03 +0100

    add function to add adalabers in my list

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,41 @@ import contacts from "../data/contacts.json";
 import { useState } from "react";
 
 function App() {
-  const [data, setData] = useState(contacts); //aqui guardamos datos iniciales, cuando cambien se irán renderizando. Como queremos obtener el valor de contacts, pues en el useState le pasamos contacts.
-  console.log(data);
 
+  //VARIABLES ESTADO
 
-  //Fución para pintar en el HTML. 
-  const htmlData = data.results.map((oneContact, index) => {
+  //Variable estado con los datos iniciales. 
+
+  const [data, setData] = useState(contacts.results); 
+
+  //Variable estado objeto para guardar los datos de las nuevas adalabers que quiero añadir. 
+
+  const [newContact, setNewContact] = useState({ 
+    name: '', 
+    counselor: '',
+    speciality: '',
+  });
+
+  //FUNCIONES
+
+  //Función manejadora para añadir un nuevo contacto. 
+
+  const handleNewContact = (ev) => {  
+    setNewContact({ ...newContact, [ev.target.id]: ev.target.value });
+  };
+
+  //Función click para que el nuevo contacto se pinte en la tabla 
+
+  const handleClick = (ev) => {  
+    ev.preventDefault(); //no queremos que se recargue la página. 
+    console.log(data.results);
+    data.push(newContact);
+    setData([...data]);
+  };
+
+  //Fución para pintar en el HTML la lista de contactos. 
+
+  const htmlData = data.map((oneContact, index) => {
     return (
       <tr key={index}>
         <td>{oneContact.name}</td>
@@ -20,6 +49,8 @@ function App() {
 
   return (
     <div>
+            <h1>Adalabers</h1>
+
       <table className="table">
         <thead>
           <tr>
@@ -32,6 +63,34 @@ function App() {
         {htmlData}
         </tbody>
       </table>
+      <form action="">
+        <h2>Añadir una adalaber</h2>
+        <label htmlFor="">Nombre:</label>
+        <input className="new-contact__input"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Ej: María"
+            onInput={handleNewContact}
+             />
+        <label htmlFor="">Tutora:</label>
+        <input className="new-contact__input"
+            type="text"
+            name="counselor"
+            id="counselor"
+            placeholder="Ej: Dayana"
+            onInput={handleNewContact}
+             />
+        <label htmlFor="">Especialidad:</label>
+        <input className="new-contact__input"
+            type="text"
+            name="speciality"
+            id="speciality"
+            placeholder="Ej: React"
+            onInput={handleNewContact}
+             />
+        <button onClick={handleClick}>Añadir una nueva Adalaber</button>
+      </form>
     </div>
   );
 }
