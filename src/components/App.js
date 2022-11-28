bb7538613a4e2a0f8353161f9d88173886f3baa2
marocena26 <mariaelena.arocena@gmail.com>
2022-11-28 08:52:41 +0100

    add structure HTML

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,39 @@
-import '../styles/App.scss';
+import "../styles/App.scss";
+import contacts from "../data/contacts.json";
 
 function App() {
+  
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <!-- Primera fila --> */}
+          <tr>
+            <td>MariCarmen</td>
+            <td>Yanelis</td>
+            <td>Python</td>
+          </tr>
+          {/* <!-- Segunda fila --> */}
+          <tr>
+            <td>Amparo</td>
+            <td>Dayana</td>
+            <td>IA</td>
+          </tr>
+          {/* <!-- Tercera fila --> */}
+          <tr>
+            <td>Escandia</td>
+            <td>Iván</td>
+            <td>3D graphics</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
